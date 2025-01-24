71815f50d8316d1ded722e30864e2c6acc282728
Vinodh <vinodh.jeevanantham@gmail.com>
2025-01-24 20:12:22 +0530

    jest conig

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
+
+export default config;
